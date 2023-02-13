3839146d012589b7106e31a684caaeeaf65b72c1
ANjitGC741 <anzit741@gmail.com>
2023-02-13 18:46:22 +0545

    assignment 1

---
+++ Anjit.java
@@ -0,0 +1,15 @@
+public class Anjit{
+   public static void main(String[] args){
+     System.out.println("");
+     System.out.println("Purna clothing center");
+     System.out.println("");
+     System.out.println("Address:");
+     System.out.println("----State: Kathmandu");
+     System.out.println("----Zip Code: 44600");
+     System.out.println("----Country: Nepal");
+     System.out.println("");
+     System.out.println("Contact Information:");
+     System.out.println("----Mobile Number:1111111111");
+     System.out.println("----Email:purna@gmail.com");
+    }
+}
